28cad8c9f069e8b21c30e98689fb0a454d40dd69
Haq Nawaz <haq.nawaz@telefonicatech.uk>
2025-02-03 17:34:45 +0000

    remove obsolete disk initialization scripts

--- initialise-and-format-disk-p09.ps1
+++
@@ -1,9 +0,0 @@
-# initialize-and-format-disk.ps1
-$diskNumber = 1
-$partitionStyle = 'GPT'
-$driveLetter = 'E'
-
-# Initialize the disk
-Initialize-Disk -Number $diskNumber -PartitionStyle $partitionStyle -PassThru |
-  New-Partition -AssignDriveLetter -UseMaximumSize |
-  Format-Volume -FileSystem NTFS -NewFileSystemLabel "DataDisk" -Confirm:$false
